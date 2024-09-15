b1f87a729fd2284cd19aa79e1a8df6b7193164fa
prachi-naktode <prachi06naktode@gmail.com>
2024-09-15 11:49:00 +0530

    edited banner

--- imbd-clone/src/components/Home/Banner.jsx
+++ imbd-clone/src/components/Home/Banner.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 export default function Banner({ movie }) {
-  let url = ""
-  url = movie ? `https://image.tmdb.org/t/p/original/${movie.backdrop_path}` : "https://www.filmofilia.com/wp-content/uploads/2010/06/inception_banner_04.jpg";
+  const url = movie ? `https://image.tmdb.org/t/p/original/${movie.backdrop_path}` : "https://www.filmofilia.com/wp-content/uploads/2010/06/inception_banner_04.jpg";
   return (   
       <div
         className="max-h-full max-w-full md:h-[75vh] bg-cover flex items-end bg-no-repeat"
